1e51b1162cd1155724463947b53619ea078a3d68
Adella <adellakenn@gmail.com>
2019-04-24 14:58:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,13 +15,13 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "WHAT DID YOU EAT TODAY?"
-    "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   elsif phrase == "WHAT?"
-    "NO, NOT SINCE 1938"
+    puts "NO, NOT SINCE 1938"
   else phrase == !phrase.uppercase
-    "HUH?!, SPEAK UP, SONNY"
+    puts "HUH?!, SPEAK UP, SONNY"
 end
   
  

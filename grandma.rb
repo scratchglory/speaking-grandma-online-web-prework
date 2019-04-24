3ba178d7cc2f9d5301862c6409cf070ad1bca8e8
Adella <adellakenn@gmail.com>
2019-04-24 14:46:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,12 +14,12 @@
 
 speak_to_grandma = phrase
 
-def speak_to_grandma()
-if speak_to_grandma == phrase
+def speak_to_grandma(phrase)
+  if speak_to_grandma == phrase
   "HUH?! SPEAK UP, SONNY!"
-elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
+  elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
-elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
+  elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
   "NO, NOT SINCE 1938!"
 else speak_to_grandma == "WHAT?"
     "NO, NOT SINCE 1938"

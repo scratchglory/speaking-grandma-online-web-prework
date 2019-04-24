19fce87b71f8cd6fae9c951e4926cf32e33b2d9a
Adella <adellakenn@gmail.com>
2019-04-24 14:48:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,6 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-speak_to_grandma = phrase
 
 def speak_to_grandma(phrase)
   if speak_to_grandma == phrase

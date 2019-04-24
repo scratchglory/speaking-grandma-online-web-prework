2ddda8b7f340b8e041fbe3cafb4102a065bee229
Adella <adellakenn@gmail.com>
2019-04-24 14:55:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrasea == "WHAT DID YOU EAT TODAY?"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
     "NO, NOT SINCE 1938!"
   elsif phrase == "WHAT?"
     "NO, NOT SINCE 1938"
